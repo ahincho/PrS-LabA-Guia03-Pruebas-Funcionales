c1b31ab241aed8efc9b4eed0d784b2194d957dae
ahincho <ahincho@unsa.edu.pe>
2023-06-24 07:58:15 -0500

    Renombrando Clases Test Runners

--- src/main/java/com/unsa/proposed/MaxTestRunner.java
+++ src/main/java/com/unsa/proposed/MaxTestRunner.java
@@ -1,12 +1,12 @@
-package com.unsa.solved;
+package com.unsa.proposed;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.Result;
 
-public class TestRunner {
+public class MaxTestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(MultiplyCheckerTest.class);
+        Result result = JUnitCore.runClasses(MaxCheckerTest.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
--- src/main/java/com/unsa/solved/MultiplyTestRunner.java
+++ src/main/java/com/unsa/solved/MultiplyTestRunner.java
@@ -1,12 +1,12 @@
-package com.unsa.proposed;
+package com.unsa.solved;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.Result;
 
-public class TestRunner {
+public class MultiplyTestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(MaxCheckerTest.class);
+        Result result = JUnitCore.runClasses(MultiplyCheckerTest.class);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
